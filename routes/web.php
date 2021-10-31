e18a5c6503010f2756b1e422b953b208b156d450
Simon Schäffer <89905047+DigitalArtis4n@users.noreply.github.com>
2021-10-31 12:02:52 +0100

    added auth with breeze

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::resource('posts', PostController::class);
+require __DIR__.'/auth.php';
